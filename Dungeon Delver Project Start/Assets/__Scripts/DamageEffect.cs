b3f16e51b18394d111f3c0e5fc243c883a60f9f9
yunting0707 <yunting0707@users.noreply.github.com>
2018-12-12 21:28:26 -0500

    Initial commit

---
+++ Dungeon Delver Project Start/Assets/__Scripts/DamageEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEffect : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public int damage = 1;
+    public bool knockback = true;
+
+// Use this for initialization
+void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
